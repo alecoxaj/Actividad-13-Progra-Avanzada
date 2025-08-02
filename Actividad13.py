d1edfa03eb411fdd3b6aac93618f2e8906e93ab5
Alejandro <alejandrocoxaj94@gmail.com>
2025-08-02 15:08:09 -0600

    Alejandro agregó la función de agregar curso.

--- Actividad13.py
+++ Actividad13.py
@@ -4,12 +4,32 @@ estudiantes = {}
 
 def agregar_estudiante():
     print("Agregar estudiante")
-    IDest = input("Ingresa el ID de estudiante: ")
-    if IDest in estudiantes:
+    idest = input("Ingresa el ID de estudiante: ")
+    if idest in estudiantes:
         print("El ID ya está registrado")
         return
     nombre = input("Ingresa tu nombre completo: ")
     carrera = input("Ingresa tu carrera o programa académico: ")
-    estudiantes[IDest] = {"nombre": nombre, "carrera": carrera, "cursos": {}}
+    estudiantes[idest] = {"nombre": nombre, "carrera": carrera, "cursos": {}}
     print("¡Estudiante agregado correctamente!")
+
+def agregar_curso():
+    print("--Agregar curso con nota--")
+    idest = input("Ingresa el ID de estudiante: ")
+    if idest not in estudiantes:
+        print("Estudiante no encontrado")
+        return
+    curso = input("Nombre del curso: ")
+    nota = input("Nota final (0-100): ")
+    try:
+        nota = float(nota)
+        if nota < 0 or nota > 100:
+            print("¡Nota fuera del rango!")
+            return
+        estudiantes[idest]["cursos"][curso] = nota
+        print("Curso y nota agregados correctamente")
+    except:
+        print("¡Nota inválida!")
+
+
 
