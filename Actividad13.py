1c5375ab5d6b9160a04c59d0ac4bb4f8db80050f
Alejandro <alejandrocoxaj94@gmail.com>
2025-08-02 15:03:13 -0600

    Alejandro agregó la función de agregar estudiantes.

--- Actividad13.py
+++ Actividad13.py
@@ -1,1 +1,15 @@
 print("¡Bienvenido al Gestor académico!")
+
+estudiantes = {}
+
+def agregar_estudiante():
+    print("Agregar estudiante")
+    IDest = input("Ingresa el ID de estudiante: ")
+    if IDest in estudiantes:
+        print("El ID ya está registrado")
+        return
+    nombre = input("Ingresa tu nombre completo: ")
+    carrera = input("Ingresa tu carrera o programa académico: ")
+    estudiantes[IDest] = {"nombre": nombre, "carrera": carrera, "cursos": {}}
+    print("¡Estudiante agregado correctamente!")
+
